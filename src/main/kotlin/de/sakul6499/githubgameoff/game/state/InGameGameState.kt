a6e78166897e6823505530b5806dbaf9cdad975f
@Sakul6499 | Lukas <sakul6499@live.de>
2017-11-05 14:31:00 +0100

    Removed unused code

--- src/main/kotlin/de/sakul6499/githubgameoff/game/state/InGameGameState.kt
+++ src/main/kotlin/de/sakul6499/githubgameoff/game/state/InGameGameState.kt
@@ -5,13 +5,10 @@ import de.sakul6499.githubgameoff.game.asset.SpriteFont
 import de.sakul6499.githubgameoff.game.gui.Button
 import java.awt.Color
 import java.awt.Graphics
-import java.awt.image.BufferedImage
 
 class InGameGameState : GameState {
     override val name: String = "InGame"
     private val button: Button = Button(200, 200, "Test!", true, 0, 0, 15, 15, 15, 15, SpriteFont.tileDimension / 2, SpriteFont.tileDimension / 2, SpriteFont.tileDimension / 4, SpriteFont.FontColor.BLACK, SpriteFont.FontType.BOLD)
-
-    private val image: BufferedImage = SpriteFont.getChar('!', SpriteFont.FontType.BOLD, SpriteFont.FontColor.WHITE)
 
     override fun update(deltaTime: Double) {
         button.update(deltaTime)
@@ -22,14 +19,6 @@ class InGameGameState : GameState {
         graphics.color = Color.BLACK
         graphics.fillRect(0, 0, GameMain.gameConfig.width, GameMain.gameConfig.height)
         // ###
-
-//        graphics.drawImage(SpriteLoader.createPlaceholderImage(), 0, 0, 128, 128, null)
-//        graphics.drawImage(image, 0, 128, 128, 128, null)
-//        graphics.drawImage(SpriteLoader.createPlaceholderImage(), 0, 128 * 2, 128, 128, null)
-//
-//        graphics.color = Color.WHITE
-//        graphics.fillRect(128, 0, 128, 128 * 3)
-//        graphics.drawImage(SpriteFont.getChar('!', SpriteFont.FontType.BOLD, SpriteFont.FontColor.BLACK), 128, 128, 128, 128, null)
 
         button.render(deltaTime, graphics)
     }
